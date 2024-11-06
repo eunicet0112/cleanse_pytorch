fe7c7d9bd0692a70a1657357004543d4fc47b543
eunicet0112 <65995562+eunicet0112@users.noreply.github.com>
2024-11-06 11:55:08 +0800

    Update visualizer_cifar.py

--- visualizer_cifar.py
+++ visualizer_cifar.py
@@ -9,7 +9,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-import utils_backdoor
+import utils_backdoor_cifar
 import os
 from decimal import Decimal
 
